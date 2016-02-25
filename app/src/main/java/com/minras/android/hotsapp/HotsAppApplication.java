8f9c26b4e422a854d943adc53df85ea20cf261d2
Minras <andrey.shchurkov@gmail.com>
2016-02-26 00:44:27 +0100

    Added application

---
+++ app/src/main/java/com/minras/android/hotsapp/HotsAppApplication.java
@@ -0,0 +1,66 @@
+package com.minras.android.hotsapp;
+
+import android.app.Activity;
+import android.app.Application;
+import android.graphics.Color;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.view.ViewGroup;
+
+
+public class HotsAppApplication
+        extends Application
+        implements MessageManager.MessageListener {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MessageManager.getInstance().subscribeMessageListener(this);
+    }
+
+    private Activity mCurrentActivity = null;
+
+    public Activity getCurrentActivity(){
+        return mCurrentActivity;
+    }
+
+    public void setCurrentActivity(Activity mCurrentActivity){
+        this.mCurrentActivity = mCurrentActivity;
+    }
+
+    /**
+     * Activity can try to unregister itself from being a current activity
+     * @param mCurrentActivity Activity object that tries to unregister itself
+     */
+    public void unsetCurrentActivity(Activity mCurrentActivity){
+        if (getCurrentActivity().equals(mCurrentActivity)) {
+            setCurrentActivity(null);
+        }
+    }
+
+    @Override
+    public void onMessageReceived(String status, String msg) {
+        int bgColor;
+        switch (status) {
+            case MessageManager.STATUS_ERROR:
+                bgColor = R.color.bgError;
+                break;
+            case MessageManager.STATUS_SUCCESS:
+                bgColor = R.color.bgSuccess;
+                break;
+            case MessageManager.STATUS_WARNING:
+                bgColor = R.color.bgWarning;
+                break;
+            case MessageManager.STATUS_NEUTRAL:
+            default:
+                bgColor = R.color.colorPrimary;
+                break;
+        }
+        CoordinatorLayout coordinatorLayout = (CoordinatorLayout) getCurrentActivity().findViewById(
+                R.id.coordinatorLayout);
+        Snackbar snackbar = Snackbar.make(coordinatorLayout, msg, Snackbar.LENGTH_LONG);
+        ViewGroup snackView = (ViewGroup) snackbar.getView();
+        snackView.setBackgroundColor(ContextCompat.getColor(snackView.getContext(), bgColor));
+        snackbar.setActionTextColor(Color.GREEN).show();
+    }
+}
---
+++ app/src/main/java/com/minras/android/hotsapp/MessageManager.java
@@ -0,0 +1,40 @@
+package com.minras.android.hotsapp;
+
+import java.util.ArrayList;
+
+public class MessageManager {
+    public static final String STATUS_ERROR = "error";
+    public static final String STATUS_WARNING = "warning";
+    public static final String STATUS_SUCCESS = "success";
+    public static final String STATUS_NEUTRAL = "neutral";
+
+    protected MessageManager() {}
+    private static MessageManager instance = null;
+    public static MessageManager getInstance() {
+        if (instance == null) {
+            instance = new MessageManager();
+        }
+        return instance;
+    }
+
+    private ArrayList<MessageListener> messageListeners = new ArrayList<>();
+
+    public interface MessageListener
+    {
+        void onMessageReceived(String status, String msg);
+    }
+
+    public void subscribeMessageListener(MessageListener listener) {
+        messageListeners.add(listener);
+    }
+
+    public void unsubscribeMessageListener(MessageListener listener) {
+        messageListeners.remove(listener);
+    }
+
+    public void sendMessage(String status, String msg) {
+        for (MessageListener listener : messageListeners) {
+            listener.onMessageReceived(status, msg);
+        }
+    }
+}
